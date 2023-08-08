c04ee3507eaebb522280adeb3f3b297dab0aeafc
shabberahmed <shabberahmed@icloud.com>
2023-08-09 00:39:41 +0530

    one

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 <a href='/'>
 <div className='d-flex align-items-center'>
   <img style={{width:'60px',height:'60px'}} src="https://icon-library.com/images/home-icon-transparent-background/home-icon-transparent-background-3.jpg" alt=""/>
-  <h1 className='text-5xl ms-3 text-neutral-950	font-light	is-family-code'>D<span className='text-yellow-200 text-5xl'>ream</span>Homes</h1>
+  <h1 className='text-4xl ms-3 text-neutral-950	font-light	is-family-code'>D<span className='text-yellow-200 text-5xl'>ream</span>Homes</h1>
   </div>
 </a>
   
